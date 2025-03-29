16bd9afd696d908c4f9158bed3481dcfdb9a3d3e
developer-ajay <developer.ajay618@gmail.com>
2025-03-29 22:32:12 +0530

    api

--- app.js
+++ app.js
@@ -16,3 +16,27 @@ let day = dateObj.getUTCDate() - 1;
 let year = dateObj.getUTCFullYear();
 
 date.innerHTML = `${month} ${day}, ${year}`;
+
+const getWeather = async () => {
+
+    try {
+        const cityName = document.getElementById('search-bar-input').value;    
+        const weatherDataFetch = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${"LOAD API KEY IN HERE"}`, {
+            headers: {
+                Accept: "application/json"
+              }
+        });  
+    
+        const weatherData = await weatherDataFetch.json();    
+        console.log(weatherData)    
+        city.innerHTML = `${weatherData.name}`;
+        description.innerHTML = `${weatherData.weather[0].main}`
+        tempImg.innerHTML = `<img src="http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png"/>`;
+        temp.innerHTML = `<h2>${Math.round(weatherData.main.temp)}°C</h2>`;    
+        tempMax.innerHTML = `${weatherData.main.temp_max}°C`;
+        tempMin.innerHTML = `${weatherData.main.temp_min}°C`;
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
